8a0d7980d3a676c9048bfa23dd21d262542b86e6
Lars Kellogg-Stedman <lars@oddbit.com>
2026-01-07 16:41:16 -0500

    Use logmsg in place of perror in must.c

--- must.c
+++ must.c
@@ -1,16 +1,18 @@
-#include "must.h"
-
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "log.h"
+#include "must.h"
 
 void _must(const char *fileName, int lineNumber, const char *funcName,
            const char *calledFunction, int err, char *msg) {
   if (err < 0) {
     char buf[256];
-    snprintf(buf, 256, "%s (%s:%d): %s failed (errno=%d): %s", funcName,
-             fileName, lineNumber, calledFunction, errno, msg);
-    perror(buf);
+    snprintf(buf, 256, "%s (%s:%d): %s failed (errno=%d): %s: %s", funcName,
+             fileName, lineNumber, calledFunction, errno, msg, strerror(errno));
+    logmsg(LOG_ERROR, buf);
     exit(1);
   }
 }
